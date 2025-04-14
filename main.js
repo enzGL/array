9b8fe74179d554e68c014e139a5cc626044e10f8
Enzo Lanzi <enzolanzi189@gmail.com>
2025-04-14 20:20:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ nomeClientes.push('Luiz')
 nomeClientes.push('Felipe')
 
 console.dir(nomeClientes)
-
-document.getElementById("conteudo").innerHTML = nomeClientes[0] + ' - ' 
-document.getElementById("conteudo").innerHTML += nomeClientes[1] + ' - '
-document.getElementById("conteudo").innerHTML += nomeClientes[2] + ' - '
+document.getElementById("conteudo").innerHTML = "<ul>"
+document.getElementById("conteudo").innerHTML += "<li>" + nomeClientes[0] + "</li>" 
+document.getElementById("conteudo").innerHTML += "<li>" + nomeClientes[1] + "</li>"
+document.getElementById("conteudo").innerHTML += "<li>" + nomeClientes[2] + "</li>"
+document.getElementById("conteudo").innerHTML += "<li>" + nomeClientes[3] + "</li>"
+document.getElementById("conteudo").innerHTML += "<li>" + nomeClientes[4] + "</li>"
+document.getElementById("conteudo").innerHTML += "</ul>"
